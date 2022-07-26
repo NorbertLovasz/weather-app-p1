e5e24d98c3dc51f269b30eb7fa1045ec44f2b1ca
unknown <lovasz.norbert.m+a@gmail.com>
2022-07-26 11:07:37 +0300

    add utility functions

---
+++ utils/weather.js
@@ -0,0 +1,8 @@
+function windToKmPerHour(meterPerSec) {
+  return (meterPerSec * 3600) / 1000;
+}
+
+// Pe baza codului iconitei, generam link-ul acesteia.
+function getWeatherIcon(iconCode) {
+  return `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
