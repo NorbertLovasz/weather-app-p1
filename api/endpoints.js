45a102222b83677d402cb0a9b2a0268011f9665e
unknown <lovasz.norbert.m+a@gmail.com>
2022-07-26 11:05:01 +0300

    add API info

---
+++ api/endpoints.js
@@ -0,0 +1,13 @@
+const API_KEY = "5c55761872c8240c69e9dfa521ae6334";
+
+// Construim link-urile (andpoint-urile) severelor de la care vom primi date.
+function getCurrentWeatherEndpoint(city) {
+  // Intotdeauna, cand folosim un API, putem obtine informatii extra prin query params.
+  // lang=ro => Rezultatele vin in romana.
+  // units=metric => Unitatea de masura va fi grade celsius.
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecastEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
